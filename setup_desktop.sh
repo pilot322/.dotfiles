715e15945df95d6a92e2ba4ea106f5b729a19b83
Michalis <michalis.lappas@outlook.com>
2025-02-21 14:42:12 +0200

    explicitly install nvim and wezterm

--- setup_desktop.sh
+++ setup_desktop.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-sudo apt install -y feh fzf stow tmux
+sudo apt install -y feh fzf stow tmux flatpak
 
 DOTFILES_DIR="$HOME/.dotfiles"
+
+# nvim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+
+# wezterm
+flatpak install flathub org.wezfurlong.wezterm
+flatpak run org.wezfurlong.wezterm
 
 for dir in "$DOTFILES_DIR"/*/; do
     dir_name=$(basename "$dir")
@@ -13,3 +22,7 @@ for dir in "$DOTFILES_DIR"/*/; do
     echo "Stowing directory: $dir_name"
     stow $dir_name
 done
+
+#.bashrc
+mv ~/.bashrc ~/bashrc.backup
+ln -s ~/.dotfiles/.bashrc ~/.bashrc
